c0a8419c870a732c735459122332cacc60360cf7
Kham1doff <xamidovbehruz66@gmail.com>
2024-06-11 15:18:32 +0500

    fayl qoshildi

---
+++ topishmoq_raqam.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 14:22:56 2024
+
+@author: HP
+"""
+topishmoq_raqam = 3
+for urinish in range (10):
+    print(urinish +1, "-urinish")
+    oyinchi_raqami = int(input("Raqamingizni kiriting: "))
+    if oyinchi_raqami > topishmoq_raqam:
+        print("Mening oylagan raqamim kichkina")
+        
+    elif oyinchi_raqami < topishmoq_raqam:
+        print("Men oylagan raqam katta")
+        
+    elif oyinchi_raqami == topishmoq_raqam:
+        print("Siz yutdingiz! Tabriklayman! Men oylagan raqamim: ", topishmoq_raqam)
+        
+    elif oyinchi_raqami != oyinchi_raqami:
+        print("Afsuski siz yutqazdingiz!")
+
+
+
+
+    
