794238876bcef0970bc4b3f632c8855ef0406b2a
Kham1doff <xamidovbehruz66@gmail.com>
2024-06-13 11:33:31 +0500

    my.py 19
    kerak

---
+++ безымянный19.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 13:57:06 2024
+
+@author: HP
+"""
+
+a = int(input("son kiriting:"))
+for i in range(1):
+    print(a)
+    print(a*2)
+    print(a*3)
+    print(a*4)
+    print(a*5)
