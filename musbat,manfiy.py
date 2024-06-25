05621d3ca7b933f3f5290d074953a3c3e0b0b375
Kham1doff <xamidovbehruz66@gmail.com>
2024-06-25 16:00:01 +0500

    Add files via upload

---
+++ musbat,manfiy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 15:35:48 2024
+
+@author: HP
+"""
+musbat = 0 
+manfiy = 0
+ 
+for i in range (3):
+    raqam = int(input())
+    if raqam > 0:
+        musbat = manfiy +1
+    elif raqam < 0:
+        musbat = manfiy + 1
+
+print(musbat,manfiy)
